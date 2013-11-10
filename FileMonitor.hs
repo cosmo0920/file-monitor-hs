397eb2e212f553cd9a6904a43510ec8937932951
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-11-10 18:20:57 +0900

    using to wait forever

--- FileMonitor.hs
+++ FileMonitor.hs
@@ -8,34 +8,40 @@ import Data.Digest.Pure.SHA
 import qualified Data.ByteString.Lazy.Char8 as BL
 import qualified Data.Text                  as T
 import Data.Text.Encoding
+import Control.Monad
+import System.Exit
 import Encode
 
 main :: IO ()
 main = do
+  hSetBuffering stdin NoBuffering
+  hFlush stdout
   dir <- getCurrentDirectory
   let path = FP.decodeString dir
-  putStrLn $ FP.encodeString path
+  putStrLn $ "[path] " ++ FP.encodeString path
   setCurrentDirectory $ FP.encodeString path
   wd <- getWorkingDirectory
-  print wd
+  putStrLn $ "[watch] " ++ show wd
   man <- startManager
-  watchTree man wd (const True) $ \event ->
-    case event of
-      Modified  dir' _ -> do
-                 contentsSHA1 <- showSHA1 dir'
-                 putStrLn $ "Modified: " ++ show contentsSHA1
-      Added     dir' _ -> do
-                 contentsSHA1 <- showSHA1 dir'
-                 putStrLn $ "Added: " ++ show contentsSHA1
-      Removed   dir' _ -> do
-                 putStrLn $ "Removed: " ++ show dir'
+  forever $ do
+    watchTree man wd (const True) $ \event ->
+      case event of
+        Modified  dir' _ -> do
+                   contentsSHA1 <- showSHA1 dir'
+                   putStrLn $ "Modified: " ++ show contentsSHA1
+        Added     dir' _ -> do
+                   contentsSHA1 <- showSHA1 dir'
+                   putStrLn $ "Added: " ++ show contentsSHA1
+        Removed   dir' _ -> do
+                   putStrLn $ "Removed: " ++ show dir'
 
-  print "press retrun to stop"
-  getLine
-  print "watching stopped, press retrun to exit"
-  stopManager man
-  getLine
-  return ()
+    print "input 'quit' to quit"
+    line <- getLine
+    when (line == "quit") $ do
+      print "watching stopped"
+      stopManager man
+      print "quit"
+      exitSuccess
 
 sha1Digest :: String -> String
 sha1Digest = showDigest . sha1 . fromStrict' . encodeUtf8 . T.pack
